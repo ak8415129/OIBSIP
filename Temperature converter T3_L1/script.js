d81b8d12012982d5bb71a8b632dd6e9ff15a973e
Ankit Kumar <ak8415129@gmail.com>
2023-10-21 23:40:39 +0530

    complete

--- Temperature converter T3_L1/script.js
+++ Temperature converter T3_L1/script.js
@@ -1,1 +1,17 @@
-const ip=parseFloat(document.getElementById('input-box').value); 
+// Function to convert temperature
+function convertTemperature() {
+    const inputTemp = parseFloat(document.getElementById("inputTemp").value);
+    const conversionType = document.getElementById("conversionType").value;
+    let result;
+
+    if (conversionType === "celsius") {
+        result = (inputTemp * 9/5) + 32;
+    } else {
+        result = (inputTemp - 32) * 5/9;
+    }
+
+    document.getElementById("result").textContent = `Result: ${result.toFixed(2)}°${conversionType === "celsius" ? "F" : "C"}`;
+}
+
+// Add event listener to the "Convert" button
+document.getElementById("convertBtn").addEventListener("click", convertTemperature);
